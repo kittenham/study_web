49ce490aef494cd000b4c09f7b539a0743f346ee
kim <kyjpjj@gmail.com>
2023-07-25 20:00:58 +0900

    ７９

---
+++ spring/src/main/java/xyz/itwill01/old/HelloMessageObject.java
@@ -0,0 +1,8 @@
+package xyz.itwill01.old;
+
+//DAO 클래스라고 생각
+public class HelloMessageObject {
+	public String getHelloMessage() {
+		return "Hello!!!";
+	}
+}
---
+++ spring/src/main/java/xyz/itwill01/old/MessagePrint.java
@@ -0,0 +1,20 @@
+package xyz.itwill01.old;
+
+public class MessagePrint {
+	void helloMessagePrint() {
+		HelloMessageObject object = new HelloMessageObject();
+		String message = object.getHelloMessage();
+		System.out.println("message = "+message);
+	}
+	
+	
+	public void hiMessagePrint() {
+		HelloMessageObject object = new HelloMessageObject();
+		String message = object.getHelloMessage();
+		System.out.println("message = "+message);
+	}
+}
+
+
+
+
