d6766f0001a6dbb2cbc40ad436699825ef2ef630
kim <kyjpjj@gmail.com>
2023-08-01 18:30:22 +0900

    83

---
+++ spring/src/main/java/xyz/itwill07/aop/AopAnnotationBean.java
@@ -0,0 +1,19 @@
+package xyz.itwill07.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AopAnnotationBean {
+	//핵심관심코드
+	public void display1() {
+		System.out.println("*** AopAnnotationBean 클래스의 display1() 메소드 호출 ***");
+	}
+	
+	public void display2() {
+		System.out.println("*** AopAnnotationBean 클래스의 display2() 메소드 호출 ***");
+	}
+	
+	public void display3() {
+		System.out.println("*** AopAnnotationBean 클래스의 display3() 메소드 호출 ***");
+	}
+}
