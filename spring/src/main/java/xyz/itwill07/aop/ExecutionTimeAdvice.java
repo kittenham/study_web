8ac543908b92b182f2cf634e57af62aa18a6f777
kim <kyjpjj@gmail.com>
2023-08-01 18:30:26 +0900

    83

--- spring/src/main/java/xyz/itwill07/aop/ExecutionTimeAdvice.java
+++ spring/src/main/java/xyz/itwill07/aop/ExecutionTimeAdvice.java
@@ -1,5 +1,23 @@
 package xyz.itwill07.aop;
 
-public class ExecutionTimeAdvice {
+import org.aspectj.lang.ProceedingJoinPoint;
 
+public class ExecutionTimeAdvice {
+	//타겟메소드의 명령이 실행되는 처리시간을 계산하여 기록하기 위한 메소드
+	public Object timeWatchLog(ProceedingJoinPoint joinPoint) { //반환형이 Object
+		
+		//타겟메소드의 명령 실행전에 동작될 명령 작성
+		long startTime=System.currentTimeMillis();
+		
+		//타겟메소드의 명령 실행 - 타겟메소드 호출
+		Object returnValue=joinPoint.proceed();
+		
+		//타겟메소드의 명령 실행후에 동작될 명령 작성
+		long endTime=System.currentTimeMillis();
+		
+		String className=joinPoint.getTarget().getClass().getSimpleName();
+		String methodName=joinPoint.getSignature().getName();
+		
+		
+	}
 }
--- spring/src/main/java/xyz/itwill07/aop/ExecutionTimeBean.java
+++ spring/src/main/java/xyz/itwill07/aop/ExecutionTimeBean.java
@@ -4,7 +4,7 @@ public class ExecutionTimeBean {
 	public void one() {
 		//System.currentTimeMillis() : 시스템의 현재 날짜와 시간에 대한 타임스템프를 반환하는 메소드
 		//타임스템프(TimeStamp) : 날짜와 시간에 대한 연산을 목적으로 날짜와 시간을 정수값으로 변환한 값 
-		//long startTime=System.currentTimeMillis();
+		//long startTime=System.currentTimeMillis(); //횡단관심코드
 		
 		long count=0;
 		for(long i=1;i<=10000000000L;i++) {
